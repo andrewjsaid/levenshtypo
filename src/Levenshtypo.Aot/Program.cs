65c368bcfc13269d42adb99d0cca888e01a4ab35
Andrew J Said <andrewjsaid@gmail.com>
2024-10-17 16:28:50 +0100

    Add AOT project to root all public entrypoints to library and verify they are AOT compatible

---
+++ src/Levenshtypo.Aot/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("This is an AOT program which roots all public methods of Levenshtypo!");
+Console.WriteLine("RUN dotnet publish for absolute confirmation that Levenshtypo works with AOT");
